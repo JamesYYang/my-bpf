bd42573e8d4ea82fcebf2b09fa0d9aef944d61b1
unknown <james.yang516@outlook.com>
2022-09-10 17:31:47 +0800

    add code to change dns package

--- ebpf/headers/helper.h
+++ ebpf/headers/helper.h
@@ -108,3 +108,76 @@ struct dns_query {
     uint16_t class;
     char name[MAX_DNS_NAME_LENGTH];
 };
+
+struct a_record {
+    struct in_addr ip_addr;
+    uint32_t ttl;
+};
+
+struct dns_response {
+   uint16_t query_pointer;
+   uint16_t record_type;
+   uint16_t class;
+   uint32_t ttl;
+   uint16_t data_length;
+};
+
+static inline void swap_mac_addresses(struct __sk_buff *skb) {
+  unsigned char src_mac[6];
+  unsigned char dst_mac[6];
+  bpf_skb_load_bytes(skb, offsetof(struct ethhdr, h_source), src_mac, 6);
+  bpf_skb_load_bytes(skb, offsetof(struct ethhdr, h_dest), dst_mac, 6);
+  bpf_skb_store_bytes(skb, offsetof(struct ethhdr, h_source), dst_mac, 6, 0);
+  bpf_skb_store_bytes(skb, offsetof(struct ethhdr, h_dest), src_mac, 6, 0);
+}
+
+#define ETH_HLEN sizeof(struct ethhdr)
+
+#define IP_SRC_OFF (ETH_HLEN + offsetof(struct iphdr, saddr))
+#define IP_DST_OFF (ETH_HLEN + offsetof(struct iphdr, daddr))
+
+static inline void swap_ip_addresses(struct __sk_buff *skb) {
+  unsigned char src_ip[4];
+  unsigned char dst_ip[4];
+  bpf_skb_load_bytes(skb, IP_SRC_OFF, src_ip, 4);
+  bpf_skb_load_bytes(skb, IP_DST_OFF, dst_ip, 4);
+  bpf_skb_store_bytes(skb, IP_SRC_OFF, dst_ip, 4, 0);
+  bpf_skb_store_bytes(skb, IP_DST_OFF, src_ip, 4, 0);
+}
+
+
+// Update IP checksum for IP header, as specified in RFC 1071
+// The checksum_location is passed as a pointer. At this location 16 bits need to be set to 0.
+static inline void update_ip_checksum(void *data, int len, uint16_t *checksum_location)
+{
+  uint32_t accumulator = 0;
+  int i;
+  for (i = 0; i < len; i += 2)
+  {
+    uint16_t val;
+    // If we are currently at the checksum_location, set to zero
+    if (data + i == checksum_location)
+    {
+      val = 0;
+    }
+    else
+    {
+      // Else we load two bytes of data into val
+      val = *(uint16_t *)(data + i);
+    }
+    accumulator += val;
+  }
+
+  // Add 16 bits overflow back to accumulator (if necessary)
+  uint16_t overflow = accumulator >> 16;
+  accumulator &= 0x00FFFF;
+  accumulator += overflow;
+
+  // If this resulted in an overflow again, do the same (if necessary)
+  accumulator += (accumulator >> 16);
+  accumulator &= 0x00FFFF;
+
+  // Invert bits and set the checksum at checksum_location
+  uint16_t chk = accumulator ^ 0xFFFF;
+  *checksum_location = chk;
+}
--- ebpf/tc_dns.c
+++ ebpf/tc_dns.c
@@ -5,12 +5,13 @@
 #include "helper.h"
 #include "bpf_endian.h"
 
+char dns_buffer[512];
+
 /* BPF perfbuf map */
 struct
 {
   __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
 } tc_dns_events SEC(".maps");
-
 
 // Parse query and return query length
 static inline int parse_query(struct __sk_buff *ctx, void *query_start, struct dns_query *q)
@@ -39,7 +40,7 @@ static inline int parse_query(struct __sk_buff *ctx, void *query_start, struct d
     {
       break;
     }
-    
+
     // If separator is zero we've reached the end of the domain query
     if (*(char *)(cursor) == 0)
     {
@@ -69,10 +70,86 @@ static inline int parse_query(struct __sk_buff *ctx, void *query_start, struct d
   return -1;
 }
 
+static inline void modify_dns_header_response(struct dns_hdr *dns_hdr)
+{
+  // Set query response
+  dns_hdr->qr = 1;
+  // Set truncated to 0
+  // dns_hdr->tc = 0;
+  // Set authorative to zero
+  // dns_hdr->aa = 0;
+  // Recursion available
+  dns_hdr->ra = 1;
+  // One answer
+  dns_hdr->ans_count = bpf_htons(1);
+}
+
+static inline void create_query_response(struct a_record *a, char *dns_buffer, size_t *buf_size)
+{
+  // Formulate a DNS response. Currently defaults to hardcoded query pointer + type a + class in + ttl + 4 bytes as reply.
+  struct dns_response *response = (struct dns_response *)&dns_buffer[0];
+  response->query_pointer = bpf_htons(0xc00c);
+  response->record_type = bpf_htons(0x0001);
+  response->class = bpf_htons(0x0001);
+  response->ttl = bpf_htonl(a->ttl);
+  response->data_length = bpf_htons((uint16_t)sizeof(a->ip_addr));
+  *buf_size += sizeof(struct dns_response);
+  // Copy IP address
+  memcpy(&dns_buffer[*buf_size], &a->ip_addr, sizeof(struct in_addr));
+  *buf_size += sizeof(struct in_addr);
+}
+
+//__builtin_memcpy only supports static size_t
+// The following function is a memcpy wrapper that uses __builtin_memcpy when size_t n is known.
+// Otherwise it uses our own naive & slow memcpy routine
+static inline void copy_to_pkt_buf(struct __sk_buff *ctx, void *dst, void *src, size_t n)
+{
+  // Boundary check
+  if ((void *)(long)ctx->data_end >= dst + n)
+  {
+    int i;
+    char *cdst = dst;
+    char *csrc = src;
+
+    // For A records, src is either 16 or 27 bytes, depending if OPT record is requested.
+    // Use __builtin_memcpy for this. Otherwise, use our own slow, naive memcpy implementation.
+    switch (n)
+    {
+    case 16:
+      __builtin_memcpy(cdst, csrc, 16);
+      break;
+
+    case 27:
+      __builtin_memcpy(cdst, csrc, 27);
+      break;
+
+    default:
+      for (i = 0; i < n; i += 1)
+      {
+        cdst[i] = csrc[i];
+      }
+    }
+  }
+}
+
+// static inline void swap_mac(uint8_t *src_mac, uint8_t *dst_mac)
+// {
+//   int i;
+//   for (i = 0; i < 6; i++)
+//   {
+//     uint8_t tmp_src;
+//     tmp_src = *(src_mac + i);
+//     *(src_mac + i) = *(dst_mac + i);
+//     *(dst_mac + i) = tmp_src;
+//   }
+// }
+
 // egress_cls_func is called for packets that are going out of the network
 SEC("classifier/egress")
 int tc_dns_func(struct __sk_buff *ctx)
 {
+  uint64_t start = bpf_ktime_get_ns();
+
   void *data_end = (void *)(unsigned long)ctx->data_end;
   void *data = (void *)(unsigned long)ctx->data;
 
@@ -119,7 +196,7 @@ int tc_dns_func(struct __sk_buff *ctx)
       // Check if header contains a standard query
       if (dns_hdr->qr == 0 && dns_hdr->opcode == 0)
       {
-        bpf_printk("DNS query transaction id %u", bpf_ntohs(dns_hdr->transaction_id));
+        // bpf_printk("DNS query transaction id %u", bpf_ntohs(dns_hdr->transaction_id));
 
         // Get a pointer to the start of the DNS query
         void *query_start = (void *)dns_hdr + sizeof(struct dns_hdr);
@@ -132,15 +209,97 @@ int tc_dns_func(struct __sk_buff *ctx)
           return TC_ACT_OK;
         }
 
-        bpf_printk("DNS record type: %i", q.record_type);
-        bpf_printk("DNS class: %i", q.class);
-        bpf_printk("DNS name: %s", q.name);
+        // bpf_printk("DNS record type: %i", q.record_type);
+        // bpf_printk("DNS class: %i", q.class);
+        // bpf_printk("DNS name: %s", q.name);
+
+        size_t buf_size = 0;
+        // Check if query matches a record in our hash table
+        struct a_record a_record;
+
+        a_record.ip_addr.s_addr = 0x846F070A;
+        a_record.ttl = 120;
+
+        // Change DNS header to a valid response header
+        modify_dns_header_response(dns_hdr);
+
+        // Create DNS response and add to temporary buffer.
+        create_query_response(&a_record, &dns_buffer[buf_size], &buf_size);
+
+        // // Start our response [query_length] bytes beyond the header
+        void *answer_start = (void *)dns_hdr + sizeof(struct dns_hdr) + query_length;
+        // // Determine increment of packet buffer
+        int tailadjust = answer_start + buf_size - data_end;
+
+        // // Adjust packet length accordingly 
+        if (bpf_skb_change_tail(ctx, tailadjust, 0))
+        {
+          bpf_printk("Adjust tail fail");
+        }
+        else
+        {
+          // Because we adjusted packet length, mem addresses might be changed.
+          // Reinit pointers, as verifier will complain otherwise.
+          data = (void *)(unsigned long)ctx->data;
+          data_end = (void *)(unsigned long)ctx->data_end;
+
+          // Copy bytes from our temporary buffer to packet buffer
+          copy_to_pkt_buf(ctx, data + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct udphdr) + sizeof(struct dns_hdr) + query_length,
+                          &dns_buffer[0], buf_size);
+
+          eth = data;
+          ip = data + sizeof(struct ethhdr);
+          udp = data + sizeof(struct ethhdr) + sizeof(struct iphdr);
+
+          // Do a new boundary check
+          if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct udphdr) > data_end)
+          {
+            return TC_ACT_OK;
+          }
+
+          // Adjust UDP length and IP length
+          uint16_t iplen = (data_end - data) - sizeof(struct ethhdr);
+          uint16_t udplen = (data_end - data) - sizeof(struct ethhdr) - sizeof(struct iphdr);
+          ip->tot_len = bpf_htons(iplen);
+          udp->len = bpf_htons(udplen);
+
+          // Swap eth macs
+          // swap_mac((uint8_t *)eth->h_source, (uint8_t *)eth->h_dest);
+          swap_mac_addresses(ctx);
+
+          // Swap src/dst IP
+          // uint32_t src_ip = ip->saddr;
+          // ip->saddr = ip->daddr;
+          // ip->daddr = src_ip;
+          swap_ip_addresses(ctx);
+
+          // Set UDP checksum to zero
+          udp->check = 0;
+
+          // Swap udp src/dst ports
+          uint16_t tmp_src = udp->source;
+          udp->source = udp->dest;
+          udp->dest = tmp_src;
+
+          // Recalculate IP checksum
+          update_ip_checksum(ip, sizeof(struct iphdr), &ip->check);
+
+          uint64_t end = bpf_ktime_get_ns();
+          uint64_t elapsed = end - start;
+          bpf_printk("Time elapsed: %d", elapsed);
+
+          // Redirecting the modified skb on the same interface to be transmitted
+          // again
+          bpf_clone_redirect(ctx, ctx->ifindex, BPF_F_INGRESS);
+
+          // We modified the packet and redirected a clone of it, so drop this one
+          return TC_ACT_SHOT;
+        }
       }
     }
   }
 
   return TC_ACT_OK;
 };
-
 
 char _license[] SEC("license") = "GPL";
